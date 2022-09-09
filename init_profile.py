e876a0385c22aaedf8f416e5023b1995fe4b2ac4
georgeSEC1 <111563036+georgeSEC1@users.noreply.github.com>
2022-09-09 20:05:25 +1000

    Add files via upload

--- init_profile.py
+++ init_profile.py
@@ -7,32 +7,6 @@ import time
 import re
 import pandas
 import os
-option = input("Preprocess valuable addresses?[y/n]: ")
-if option == "y":
-    print("Transactions_for_address")
-    with open("blockchair_bitcoin_addresses_and_balance_LATEST.tsv", encoding='UTF-8') as f:
-            lines = f.readlines()
-    i = 0
-    threshold = input("threshold above range(e.g 10000000000): ")
-    output_path='previous_transactions_detail.csv'
-    for line in lines:
-        proc = line.split("\t")
-        if len(proc) > 1 and i > 0 and line.find("-") == -1:
-            if int(proc[1]) > int(threshold):          
-                your_btc_address = proc[0]
-                transactions_url = 'https://chain.api.btc.com/v3/address/' + your_btc_address +'/tx'
-                df = pandas.read_json(transactions_url)
-                if i == 1:
-                    df.to_csv(output_path, mode='a', header=not os.path.exists(output_path))
-                if i > 1:
-                    df.to_csv(output_path, mode='a', header=False)
-                print(i,"/",len(lines))# monitor transactions for stock movements
-                time.sleep(1)
-        i+=1
-time.sleep(5)
-print("Prepare_profile")
-subprocess.Popen("curl -s \"https://poloniex.com/public?command=return24hVolume\" -o proc.conf")
-time.sleep(5)
 def produce(string):
     array = string.split(",")
     i = 0
@@ -53,89 +27,129 @@ def produceaddr(string):
             db.append(proc)
             break
     return db
-subprocess.Popen("del *.btc /q",shell=True)
-with open("previous_transactions_detail.csv", encoding='UTF-8') as f:
-    lines = f.readlines()
-i = 0
-instance = str(random.randint(0,10000000))
-time.sleep(5)
-unixT = input("Unix time, above range(e.g 1657999140): ")
-for line in lines:
-    if line.find("\'block_time\': ") > -1:
-        array = produce(line)
-        for segment in array:
-            if int(segment) > int(unixT):#unix time specification, assign to blockchain time
-                addrs = produceaddr(line)
-                f = open("bitcoin_contempory_addr_associate.btc", "a", encoding="utf8")
-                for addr in addrs:
-                    f.write(addr + line)
-    print(str(i)+"\\"+ str(len(lines)))
-    i += 1
-time.sleep(5)
-subprocess.Popen("del *.dat /q",shell=True)
-with open('proc.conf') as f:
-    string = f.read()
-for line in string.split(","):
-    if line.find("_") > -1 and line.find("BTC") > -1:
-        proc = line.split("\"")[1]
-        print (proc)
-        subprocess.Popen("curl -s \"https://api.poloniex.com/markets/"+proc+"/candles?interval=DAY_3\"" + " -o " + proc + "_" + instance +".dat")
-time.sleep(5)
-subprocess.Popen("dir /b *.dat > market.conf",shell=True)
-time.sleep(5)
-print("0 = undecided, 1 = fall, 2 = rise")
-with open('market.conf') as f:
-    lines = f.readlines()
-statA = 0
-statB = 0
-for line in lines:
-    with open(line.strip()) as f:
-        linesX = f.readlines()
-        for lineX in linesX:
-            proc = lineX.split(",")
-            valA = re.sub('\W+',' ',proc[2])
-            valB = re.sub('\W+',' ',proc[3])
-            print(line.strip() + ":")
-            if valA < valB:
-                print(2)
-                statA+=1
-            if valA > valB:
-                print(1)
-                statB+=1
-            if valA == valB:
-                print(0)
-#copyright - george wagenknecht - 2022 - all rights reserved
-time.sleep(5)
-btcA = input("BTC above amount(e.g 100000): ")
-def produce(string):
-    array = string.split(",")
+os.system('CLS')
+option = input("Preprocess valuable addresses?[y/n]: ")
+if option == "y":
+    with open("blockchair_bitcoin_addresses_and_balance_LATEST.tsv", encoding='UTF-8') as f:
+            lines = f.readlines()
     i = 0
-    db = []
-    temp = 0
-    for segment in array:
-        if segment.find("\'value\': ") > -1:
-            db.append(int(segment.split(":")[1]))
-    return db
-def getValuesOverX(seg):
-    lines = seg.split(",")
-    i = 0
+    threshold = 1000000000#input("threshold above range(e.g 10000000000): ")
+    output_path='previous_transactions_detail.csv'
     for line in lines:
-        if line.find("\'value\': ") > -1:
+        proc = line.split("\t")
+        if len(proc) > 1 and i > 0 and line.find("-") == -1:
+            if int(proc[1]) > int(threshold):          
+                your_btc_address = proc[0]
+                transactions_url = 'https://chain.api.btc.com/v3/address/' + your_btc_address +'/tx'
+                df = pandas.read_json(transactions_url)
+                if i == 1:
+                    df.to_csv(output_path, mode='a', header=not os.path.exists(output_path))
+                if i > 1:
+                    df.to_csv(output_path, mode='a', header=False)
+                os.system('CLS')
+                print("Retrieving address transactions:",i,"/",len(lines))# monitor transactions for stock movements
+        i+=1
+time.sleep(5)
+unixB = int(time.time())-2629743#1 month ago
+xx = 1
+while(True):
+    os.system('CLS')
+    print("Begin round", str(xx))
+    time.sleep(5)
+    subprocess.Popen("del *.btc /q",shell=True)
+    with open("previous_transactions_detail.csv", encoding='UTF-8') as f:
+        lines = f.readlines()
+    time.sleep(1)
+    os.system('CLS')
+    print("Preparing profile")
+    subprocess.Popen("curl -s \"https://poloniex.com/public?command=return24hVolume\" -o proc.conf")
+    i = 0
+    instance = str(random.randint(0,10000000))
+    time.sleep(5)
+    os.system('CLS')
+    f = open("bitcoin_contempory_addr_associate.btc", "a", encoding="utf8")
+    print("Analysing addresses")
+    for line in lines:
+        if line.find("\'block_time\': ") > -1:
             array = produce(line)
             for segment in array:
-                if int(segment) > int(btcA):#btc amount
-                    i+=1
-    return str(i)
-i = 0
-with open("bitcoin_contempory_addr_associate.btc", encoding='UTF-8') as f:#e.g "bitcoin_contempory_addr_associate_4920994.dat"
-    procX = f.readlines()
+                if int(segment) > int(unixB) and int(segment) < int(unixB)+86400:#unix time specification, assign to blockchain time
+                    addrs = produceaddr(line)
+                    for addr in addrs:
+                        f.write(addr + line)
+        i += 1
+    time.sleep(5)
+    subprocess.Popen("del *.dat /q",shell=True)
+    with open('proc.conf') as f:
+        string = f.read()
+    for line in string.split(","):
+        if line.find("_") > -1 and line.find("BTC") > -1:
+            proc = line.split("\"")[1]
+            os.system('CLS')
+            print ("Loading" ,proc)
+            subprocess.Popen("curl -s \"https://api.poloniex.com/markets/"+proc+"/candles?interval=HOUR_1\"?&startTime="+str(unixB) +"&endTime="+ str(unixB + 86400) + " -o " + proc + "_" + instance +".dat")
+    time.sleep(5)
+    subprocess.Popen("dir /b *.dat > market.conf",shell=True)
+    time.sleep(5)
+    with open('market.conf') as f:
+        lines = f.readlines()
+    statA = 0
+    statB = 0
+    os.system('CLS')
+    print("Analysing market movement")
+    for line in lines:
+        with open(line.strip()) as f:
+            linesX = f.readlines()
+            for lineX in linesX:
+                proc = lineX.split(",")
+                valA = re.sub('\W+',' ',proc[2])
+                valB = re.sub('\W+',' ',proc[3])
+                if valA < valB:
+                    statA+=1
+                if valA > valB:
+                    statB+=1
+    #copyright - george wagenknecht - 2022 - all rights reserved
+    time.sleep(5)
+    btcA = 10000#input("BTC above amount(e.g 100000): ")
+    def produce(string):
+        array = string.split(",")
+        i = 0
+        db = []
+        temp = 0
+        for segment in array:
+            if segment.find("\'value\': ") > -1:
+                db.append(int(segment.split(":")[1]))
+        return db
+    def getValuesOverX(seg):
+        lines = seg.split(",")
+        i = 0
+        for line in lines:
+            if line.find("\'value\': ") > -1:
+                array = produce(line)
+                for segment in array:
+                    if int(segment) > int(btcA):#btc amount
+                        i+=1
+        return str(i)
+    i = 0
+    with open("bitcoin_contempory_addr_associate.btc", encoding='UTF-8') as f:#e.g "bitcoin_contempory_addr_associate_4920994.dat"
+        procX = f.readlines()
+    with open("proc.conf", encoding='UTF-8') as f:
+        lines = f.readlines()
+        var = ""
+        for line in lines:
+            if line.find("totalBTC") > -1:
+                var = (line[16:len(line)-3])
+                break
     for proc in procX:
         f = open("test.csv", "a", encoding="utf8")
-        if statA > statB:
-            f.write(str(int(time.time()))+","+getValuesOverX(proc) + ",0\n")
-        if statA < statB:
-            f.write(str(int(time.time()))+","+getValuesOverX(proc) + ",1\n")
+        if statA >= statB:
+            f.write(str(int(time.time())) +"," + var +","+ getValuesOverX(proc) + ",0\n")
+        if statA <= statB:
+            f.write(str(int(time.time())) +"," + var +","+ getValuesOverX(proc) + ",1\n")
         i+=1
-print("Training data constructed")
-time.sleep(5)
+    os.system('CLS')
+    print("Training data constructed")
+    unixB += 86400
+    xx+=1
+    time.sleep(1)
 
--- nn.py
+++ nn.py
@@ -6,31 +6,31 @@ from tensorflow.keras.layers import Dense
 # load the dataset
 option = input("train or predict?[t/p]:")
 if option == "t":
-    dataset = loadtxt('trainingdata.csv', delimiter=',')
+    dataset = loadtxt('test.csv', delimiter=',')
     # split into input (X) and output (y) variables
-    X = dataset[:,0:5]
-    y = dataset[:,5]
+    X = dataset[:,0:3]
+    y = dataset[:,2]
     # define the keras model
     model = Sequential()
-    model.add(Dense(12, input_shape=(5,), activation='relu'))
-    model.add(Dense(5, activation='relu'))
+    model.add(Dense(12, input_shape=(3,), activation='relu'))
+    model.add(Dense(3, activation='relu'))
     model.add(Dense(1, activation='sigmoid'))
     # compile the keras model
     model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
     # fit the keras model on the dataset
-    model.fit(X, y, epochs=1500, batch_size=10, verbose=1)
+    model.fit(X, y, epochs=500, batch_size=10, verbose=1)
     model.save('my_model')
 if option == "p":
-    dataset = loadtxt('profile.csv', delimiter=',')
+    dataset = loadtxt('realtime.csv', delimiter=',')
     # split into input (X) and output (y) variables
-    X = dataset[:,0:5]
+    X = dataset[:,0:3]
     model = keras.models.load_model('my_model')
     # make class predictions with the model
     predictions = (model.predict(X) > 0.5).astype(int)
     # summarize the first 5 cases
-    datasetB = loadtxt('trainingdata.csv', delimiter=',')
+    datasetB = loadtxt('realtime.csv', delimiter=',')
     # split into input (X) and output (y) variables
-    X = dataset[:,0:5]
-    yB = datasetB[:,5]
-    for i in range(5):
-        print('%s => %d (expected %d)' % (X[i].tolist(), predictions[i], yB[i]))
+    X = dataset[:,0:3]
+    yB = datasetB[:,2]
+    for i in range(len(dataset)):
+        print('%s => %d ' % (X[i].tolist(), predictions[i]))
