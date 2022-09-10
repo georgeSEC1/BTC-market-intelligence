d8f6e943d1d55cf1eabcd1650a74dc5338532506
georgeSEC1 <111563036+georgeSEC1@users.noreply.github.com>
2022-09-11 08:13:46 +1000

    Add files via upload

--- nn.py
+++ nn.py
@@ -41,7 +41,7 @@ while(True):
         model = keras.models.load_model('my_model')
         predictions = (model.predict(X) > 0.5).astype(int)
         i = 0
-        print ("Candle volume category & movement indicator:")
+        print ("Volume category & movement indicator:")
         while(i < len(dataset)):
             print('%s => %d' % (X[i].tolist(), predictions[i]))
             i+=1
